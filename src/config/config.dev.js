cf1fc3e1b40e0f39f27f0ddba646d83bb186bddd
662 <lya121@gmail.com>
2018-04-12 11:17:14 +0800

    zhushi

--- src/config/config.dev.js
+++ src/config/config.dev.js
@@ -1,7 +1,7 @@
 module.exports = {
-  serve: 'http://127.0.0.1:8080',
-  keys: ['some secret hurr'],
+  serve: 'http://192.168.11.220:88',
+  keys: ['maple123'],
   session: {
     key: 'koa:sess'
   }
-}
+};
